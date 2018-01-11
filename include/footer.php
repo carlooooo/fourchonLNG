500df5b72ea8eb9895274ce6414907926887c60b
Carlo Del Valle <delvallecarlo@gmail.com>
2018-01-11 16:54:46 +0800

    include

---
+++ include/footer.php
@@ -0,0 +1,47 @@
+<footer class="footer-bs">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-6 col-sm-3 footer-brand animated fadeInLeft">
+              <h5><img src="img/logo2.png"> FOURCHON LNG</h5>
+                <p>© Fourchon LNG 2018</p>
+            </div>
+          <div class="col-xs-6 col-sm-3 footer-nav animated fadeInUp">
+              <h4>Navigation —</h4>
+              
+              <div class="col-xs-6 col-sm-6">
+                    <ul class="list">
+                        <li><a href="#">Project Description</a></li>
+                        <li><a href="#">Project Location</a></li>
+                        <li><a href="#">About LNG</a></li>
+                        <li><a href="#">The FERC Process</a></li>
+                    </ul>
+                </div>
+           </div>
+          <div class="col-xs-6 col-sm-3 footer-nav animated fadeInUp">
+              <h4></h4>
+              <div class="col-md-6 col-md-6">
+                    <ul class="list">
+                        <li><a href="#">Safety and the Environment</a></li>
+                        <li><a href="#">Community Outreach</a></li>
+                        <li><a href="#">Links</a></li>
+                    </ul>
+                </div>
+           </div>
+          <div class="col-xs-6 col-sm-3 footer-ns animated fadeInRight">
+              <h4>Newsletter</h4>
+                <p>Subscribe to our newsletter.</p>
+                <p>
+                    <div class="input-group">
+                      <input type="text" class="form-control" placeholder="Email address">
+                      <span class="input-group-btn">
+                        <button class="btn btn-default" type="button"><span class="glyphicon glyphicon-envelope"></span></button>
+                      </span>
+                    </div><!-- /input-group -->
+                 </p>
+            </div>
+        </div>
+      </div>
+      </footer>
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
---
+++ index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include('include/header.php'); ?>
+
+  <body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="#"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav m2-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Project Descrpition</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Project Location</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About LNG</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">The FERC Process</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Safety and the Environment</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Community Outreach</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Links</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+
+      <div class="row">
+
+        <div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel">
+            <ol class="carousel-indicators">
+              <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner" role="listbox">
+              <div class="carousel-item active">
+                <img class="d-block img-fluid" src="img/slide1b.jpg" alt="First slide">
+              </div>
+              <div class="carousel-item">
+                <img class="d-block img-fluid" src="img/slide1.jpg" alt="Second slide">
+              </div>
+              
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
+
+        <div class="col-sm-8">
+          <h2 class="mt-4">Fourchon LNG</h2>
+          <p>Please note:- this website is under development and will be updated on a regular basis as the project moves forward. </p>
+          <p>Fourchon LNG is an exciting new project which will see the development of up to 5 million tons of Liquified Natural Gas (LNG) production per annum with associated storage and shipping facilities. </p>
+          <p>LNG from the Fourchon LNG project will serve the American Domestic market first and then be available for Export. Fourchon LNG is committed to providing local employment and sourcing of USA manufactured equipment. </p>
+          <p>Fourchon LNG invites the public to the South Lafourche Library at 16241 E. Main Street in Cut-Off, LA, for an open house on September 19th. The doors will be open from 5 p.m. to 7 p.m. This open house is being held to formally introduce the project to the public. </p>
+          <!--<p>
+            <a class="btn btn-primary btn-lg" href="#">Call to Action &raquo;</a>
+          </p>-->
+        </div>
+        <div class="col-sm-4">
+             <h2 class="mt-4">Contact Us</h2>
+          <address>
+            <strong>Fourchon LNG LLC.</strong>
+            <br>2223 S 25th Street 
+            <br>Fort Pierce, Florida 
+            <br>34986, USA 
+            <br>
+          </address>
+          <address>
+            <strong>Graham Elliott - Project Director</strong>
+            <br><abbr title="Email">Email:</abbr>
+            <a href="mailto:#">grahame@fourchonLNG.net</a>
+            <br><br><strong>Chris Pope - Administration and Outreach</strong>
+            <br><abbr title="Email">Email:</abbr>
+            <a href="mailto:#">outreach@fourchonLNG.net</a>
+          </address>
+        </div>
+      </div>
+      <!-- /.row -->
+      </div>
+      <!-- /.row -->
+    </div>
+    <!-- /.container -->
+    <!-- Footer -->
+    <?php include('include/footer.php'); ?>
+
+  </body>
+
+</html>
